101c0ba02ea98b1439e95a0cdfc49a4fa2466618
Marie-Ève Noël <67923392+nonowelle@users.noreply.github.com>
2020-12-13 16:41:09 -0500

    made the container responsive

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const accordion = document.getElementsByClassName("contentBox");
+const accordion = document.getElementsByClassName('contentBox');
 
 for (i = 0; i < accordion.length; i++) {
-    accordion[i].addEventListener('click', () => {
+    accordion[i].addEventListener('click', function () {
         this.classList.toggle('active')
     })
 }
