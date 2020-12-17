34d0e61c64cc81bf7ae7b0cac0d7ddf17c5e115e
Marie-Ève Noël <67923392+nonowelle@users.noreply.github.com>
2020-12-17 16:23:44 -0500

    toggle accordion

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-const accordion = document.getElementsByClassName('contentBox');
-
-
-
-for (i = 0; i < accordion.length; i++) {
-    accordion[i].addEventListener('click', function () {
-        this.classList.toggle('active')
-        removeSelection(accordion[i], accordion);
-
-    })
+function addClass(el, klass) {
+    el.classList.add(klass);
 }
 
+function removeClass(el, klass) {
+    el.classList.remove(klass);
+}
 
+const accordionItems = document.querySelectorAll(".contentBox")
+const content = document.querySelectorAll('.content');
+
+accordionItems.forEach(function (accordion) {
+    const label = accordion.firstElementChild;
+    label.addEventListener('click', toggleAccordion);
+});
+
+
+function toggleAccordion(e) {
+    content.forEach(function (text) {
+
+        if (text.previousElementSibling === e.target) {
+            addClass(text.parentElement, 'active');
+        } else {
+            removeClass(text.parentElement, 'active');
+
+        }
+    });
+}
